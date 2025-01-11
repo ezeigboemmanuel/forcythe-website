f7b676a57e6ec51fd23987efdeafc0b8d5459f83
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2025-01-11 08:48:08 +0100

    Desktop navbar done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,25 +1,43 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="bg-[#030516]">
-      <div>
-        <Image
-          src="/assets/forcythelogo.svg"
-          alt="logo"
-          width={150}
-          height={150}
-        />
-        <ul className="text-sm">
-          <li>About</li>
-          <li>Services</li>
-          <li>Portfolio</li>
-          <li>Studio</li>
-          <li>Foundation</li>
+    <nav className="flex justify-between items-center bg-[#030516] px-24 h-[100px] backdrop-blur-md">
+      <div className="flex space-x-16 items-center">
+        <Link href="/">
+          <Image
+            src="/assets/forcythelogo.svg"
+            alt="logo"
+            width={150}
+            height={150}
+          />
+        </Link>
+        <ul className="flex space-x-4">
+          <Link href="/about">
+            <li>About</li>
+          </Link>
+          <Link href="/services">
+            <li>Services</li>
+          </Link>
+          <Link href="/services#portfolio">
+            <li>Portfolio</li>
+          </Link>
+          <Link href="/studio">
+            <li>Studio</li>
+          </Link>
+          <Link href="/foundation">
+            <li>Foundation</li>
+          </Link>
         </ul>
       </div>
 
-      <div></div>
+      <div className="group">
+        <div className="w-[134px] h-12 border border-dashed border-white relative rounded-full  group-hover:border-[#0066cc]"></div>
+        <button className="absolute top-5 right-[90px] font-semibold text-black bg-white w-[134px] h-12 rounded-full group-hover:bg-[#0066cc] group-hover:text-white">
+          Book a Call
+        </button>
+      </div>
     </nav>
   );
 };
