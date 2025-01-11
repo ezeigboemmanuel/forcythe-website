aa8e8da7455232983407dabdf5efd2a037e9438c
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2025-01-11 13:06:53 +0100

    Feat: Hero section done

---
+++ components/Hero.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import { Play } from "lucide-react";
+
+const Hero = () => {
+  return (
+    <div className="relative w-full h-auto bg-[#030516]">
+      <div className="absolute inset-0 z-10 top-28">
+        <Image
+          src="/assets/mapbackground.svg"
+          fill
+          alt="background-img"
+          className="object-cover object-center"
+        />
+      </div>
+
+      <div className="h-auto pt-28 md:pt-28 pb-12 md:pb-20 flex flex-col px-4 md:px-12 xl:px-24 items-center">
+        <div className="bg-white bg-opacity-10 rounded-[33px] md:rounded-[46px] w-full">
+          <div className="px-4 py-6 md:p-10 xl:p-12">
+            <h1 className="text-[56px] md:text-[80px] z-50 mb-7 leading-[1] max-w-[900px]">
+              We build <span className="text-[#60a6e7]">products</span> that{" "}
+              {/* <br className="hidden md:inline-block" /> */}
+              shape a better future
+            </h1>
+            <p className="z-50 text-[#aea9b1] mb-8 md:text-lg max-w-3xl">
+              We’re the architects of digital excellence across industries. We
+              redefine business with cutting-edge digital strategies that
+              invokes sector-wide transformation.
+            </p>
+            <div className="group relative z-30 inline-block">
+              <div className="w-[144px] h-12 border border-dashed border-white rounded-full  group-hover:border-[#0066cc]"></div>
+              <button className="absolute left-1 -top-1 lg:right-[90px] font-semibold text-black bg-white w-[144px] h-12 rounded-full group-hover:bg-[#154b81] group-hover:text-white transition ease-in-out delay-150 flex items-center justify-center">
+                Book a Call <Play className="w-4 h-4 ml-2 fill-black group-hover:fill-white" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col py-10 md:py-0 pb-2 md:-mt-4 px-4 md:px-12 lg:px-24 justify-end items-center">
+        <p className="text-[32px] lg:text-[42px] mx-auto leading-[1.3] text-center">
+          Success in <span className="text-[#60a6e7]">Motion</span> - Our
+          clients’ journey
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className="flex justify-between space-x-4 items-center bg-[#030516] px-4 md:px-12 lg:px-24 h-[105px] backdrop-blur-md">
+    <nav className="flex justify-between space-x-4 items-center px-4 md:px-12 xl:px-24 h-[105px] backdrop-blur-md fixed z-50 top-0 left-0 right-0 mx-auto">
       <Link href="/" className="md:hidden">
         <Image
           src="/assets/forcythelogo.svg"
@@ -25,7 +25,7 @@ const Navbar = () => {
         </button>
 
         {isOpen && (
-          <ul className="flex flex-col space-y-6 fixed top-28 right-5 py-11 px-5 bg-gradient-to-b from-[#0c2645] via-[#000000] to-[#000000] w-60 rounded-[33px]">
+          <ul className="flex flex-col space-y-6 fixed z-50 top-28 right-5 py-11 px-5 bg-gradient-to-b from-[#0c2645] via-[#000000] to-[#000000] w-60 rounded-[33px]">
             <Link href="/about">
               <li>About</li>
             </Link>
@@ -79,9 +79,9 @@ const Navbar = () => {
         </ul>
       </div>
 
-      <div className="hidden md:inline-block group">
-        <div className="w-[134px] h-12 border border-dashed border-white rounded-full  group-hover:border-[#0066cc]"></div>
-        <button className="absolute right-[42px] top-6 lg:right-[90px] font-semibold text-black bg-white w-[134px] h-12 rounded-full group-hover:bg-[#154b81] group-hover:text-white transition ease-in-out delay-150">
+      <div className="hidden md:inline-block group relative z-50">
+        <div className="w-[132px] h-12 border border-dashed border-white rounded-full  group-hover:border-[#0066cc]"></div>
+        <button className="absolute left-1 -top-1 lg:right-[90px] font-semibold text-black bg-white w-[132px] h-12 rounded-full group-hover:bg-[#154b81] group-hover:text-white transition ease-in-out delay-150 flex items-center justify-center">
           Book a Call
         </button>
       </div>
