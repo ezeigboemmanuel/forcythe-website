40857032c7d7a9e6fcef875aa468cb36a5bd4d8b
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2025-01-11 09:10:47 +0100

    responsive: tablet nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,8 +3,18 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center bg-[#030516] px-24 h-[100px] backdrop-blur-md">
-      <div className="flex space-x-16 items-center">
+    <nav className="flex justify-between space-x-4 items-center bg-[#030516] px-12 lg:px-24 h-[105px] backdrop-blur-md">
+      <Link href="/" className="md:hidden">
+        <Image
+          src="/assets/forcythelogo.svg"
+          alt="logo"
+          width={150}
+          height={150}
+        />
+      </Link>
+      <div className="md:hidden"></div>
+
+      <div className="hidden md:flex space-x-16 items-center">
         <Link href="/">
           <Image
             src="/assets/forcythelogo.svg"
@@ -32,9 +42,9 @@ const Navbar = () => {
         </ul>
       </div>
 
-      <div className="group">
+      <div className="hidden md:inline-block group">
         <div className="w-[134px] h-12 border border-dashed border-white relative rounded-full  group-hover:border-[#0066cc]"></div>
-        <button className="absolute top-5 right-[90px] font-semibold text-black bg-white w-[134px] h-12 rounded-full group-hover:bg-[#0066cc] group-hover:text-white">
+        <button className="absolute right-[42px] top-6 lg:right-[90px] font-semibold text-black bg-white w-[134px] h-12 rounded-full group-hover:bg-[#154b81] group-hover:text-white transition ease-in-out delay-150">
           Book a Call
         </button>
       </div>
